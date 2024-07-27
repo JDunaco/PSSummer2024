586b395dcd171247973290298b4fca07dec87c1c
JDunaco <DunawayJordanR@gmail.com>
2024-07-26 23:58:24 -0500

    Crafting Base

--- Assets/GlobalScripts/Game.gd
+++ Assets/GlobalScripts/Game.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var items = [0,0,0,0,0,0,0,0]
+var crafting = false
---
+++ Assets/Ingredients/Draggable Sprite/dragable_item_2.gd
@@ -0,0 +1,42 @@
+extends Sprite2D
+
+var is_draggable = false #state Management
+var mouse_offset #center mouse
+var delay = 10
+var positionInitial = Vector2(400,120)
+
+func _ready():
+	self.position = positionInitial
+
+func _physics_process(delta):
+	if is_draggable == true:
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", get_global_mouse_position(), delay * delta)
+	else :
+		_checkRange1()
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			if get_rect().has_point(to_local(event.position)):
+				print("clicked")
+				is_draggable = true
+		else : 
+			is_draggable = false
+
+func _checkRange1():
+	if(self.position.x > 150 && self.position.x < 150+128) :
+		if(self.position.y > 450 && self.position.y < 450+128) :
+			is_draggable = false
+			self.position = Vector2(220,520)
+	else:
+		_checkRange2()
+
+func _checkRange2():
+	if(self.position.x > 500 && self.position.x < 500+128) :
+		if(self.position.y > 450 && self.position.y < 450+128) :
+			is_draggable = false
+			self.position = Vector2(570,520)
+	else:
+		is_draggable = false
+		self.position = positionInitial
